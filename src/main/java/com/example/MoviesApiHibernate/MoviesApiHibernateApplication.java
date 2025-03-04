fb915e1bfe35bffe2045b8e0583b9724d8e7c1af
Julia Toifl <julia.toifl@squer.io>
2025-03-04 21:29:58 +0100

    init commit

---
+++ src/main/java/com/example/MoviesApiHibernate/MoviesApiHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.MoviesApiHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApiHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApiHibernateApplication.class, args);
+	}
+
+}
